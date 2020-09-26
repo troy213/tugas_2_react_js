35e3cadec32ebea3ae23ca37f5fd5bced920e714
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-09-26 16:35:31 +0700

    gitignore

--- src/Page/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Footer(props){
-  return(
-    <b>Tugas 2 React Js</b>
-  );
-}
-
-export default Footer;
--- src/Page/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header(props){
-  return(
-    <center><b>Home | Product | Kontak | Tentang Kami</b></center>
-  );
-}
-
-export default Header;
